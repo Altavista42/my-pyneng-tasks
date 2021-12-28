bd606824817dad1a9afa067ea7a18c7a32073983
Altavista42 <AUSemchenko@mail.ru>
2021-12-29 00:39:31 +0300

    Выполнено задание task_6_2a.py

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,41 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input("Введите IP-адрес в формате \"10.10.10.10\": ")
+ip_check = ip.split(".")
+ip_list = []
+count_iter = 0
+count_broad = 0
+count_unas = 0
+ip_len = len(ip_check)
+
+for value in ip_check:
+    if ip_len == 4 and value.isdigit() == True and int(value) in range(0, 256):
+        count_iter += 1
+        if count_iter == 4:
+            for string in ip_check:
+                ip_list.append(int(string))
+            for num in ip_list:
+                if num in range(1, 224):
+                    print("unicast")
+                    break
+                elif num in range(224, 240):
+                    print("multicast")
+                    break
+                elif num == 255:
+                    count_broad += 1
+                    if count_broad == 4:
+                        print("local broadcast")
+                        break
+                elif num == 0:
+                    count_unas += 1
+                    if count_unas == 4:
+                        print("unassigned")
+                        break
+                else:
+                    print("unused")
+                    break
+    else:
+        print("Неправильный IP-адрес")
+        break
+
