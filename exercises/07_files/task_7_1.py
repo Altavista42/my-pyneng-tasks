93fa33bd7418463dc978a6b051d96d9ec92b31d9
Altavista42 <AUSemchenko@mail.ru>
2022-01-03 13:14:31 +0300

    Выполнено задание task_7_1.py

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,15 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+ospf_template = """
+{:<18}    {:<18}
+{:<18}    {:<18}
+{:<18}    {:<18}
+{:<18}    {:<18}
+{:<18}    {:<18}
+"""
+with open("ospf.txt", "r") as ospf:
+    for line in ospf:
+        new_list = line.replace(",", "").split()
+        print(ospf_template.format("Prefix", new_list[1], "AD/Metric", new_list[2][1:7], "Next-Hop", new_list[4],
+                                   "Last update", new_list[5], "Outbound Interface", new_list[6]))
