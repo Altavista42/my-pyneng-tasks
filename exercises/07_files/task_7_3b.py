2d82d1d2f488b617eaec2150d3b5e93e483978f7
Altavista42 <AUSemchenko@mail.ru>
2022-01-03 19:55:15 +0300

    Выполнено задание task_7_3b.py

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,17 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+num_vlan = int(input("Enter VLAN number: "))
+sort_list = []
+with open("CAM_table.txt", "r") as cam_table:
+    for line in cam_table:
+        new_list = line.split()
+        list_len = len(new_list)
+        if list_len > 2:
+            if new_list[0].isdigit() and int(new_list[0]) == num_vlan:
+                new_list.remove("DYNAMIC")
+                vlan = new_list[0]
+                mac = new_list[1]
+                port = new_list[2]
+                print(f"{vlan:<4}     {mac:<14}      {port:<5}")
+
