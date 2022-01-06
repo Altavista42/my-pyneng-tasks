15e959fefce8ded475b16b0cd85dd54e16b2a529
r2d2 <r2d2@deb-11-test.jamer.net>
2022-01-06 14:17:29 +0300

    Выполнено задание task_6_3.py

--- exercises/06_control_structures/task_6_3.py
+++ exercises/06_control_structures/task_6_3.py
@@ -74,13 +74,6 @@ trunk = {
     "0/7": ["only", "30"],
 }
 
-# for intf, vlan in access.items():
-#     print("interface FastEthernet" + intf)
-#     for command in access_template:
-#         if command.endswith("access vlan"):
-#             print(f" {command} {vlan}")
-#         else:
-#             print(f" {command}")
 
 
 keys = list(trunk.keys())
