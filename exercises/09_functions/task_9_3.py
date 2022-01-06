349876e6a5fda037ae14b9f5a4cff418b31b72b4
Altavista42 <AUSemchenko@mail.ru>
2022-01-06 22:22:54 +0300

    Выполнено задание task_9_3.py

--- exercises/09_functions/task_9_3.py
+++ exercises/09_functions/task_9_3.py
@@ -23,3 +23,28 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+from sys import argv
+config_filename = argv[1]
+
+def get_int_vlan_map(config_filename):
+    dict_access = {}
+    dict_trunk = {}
+    with open(config_filename) as config:
+         new_list = config.read().replace('\n',' ').split('!')
+         for line in new_list:
+             if 'access' in line:
+                 intf_access = line.split()[1]
+                 vlan_access = int(line.split()[-3])
+                 dict_access[intf_access] = vlan_access
+             if 'trunk' in line:
+                 intf_trunk = line.split()[1]
+                 dict_trunk[intf_trunk] = [int(item) for item in line.replace(' ',',').split(",") if item.isdigit()]
+    return dict_access, dict_trunk
+
+
+
+
+
+
+
+
