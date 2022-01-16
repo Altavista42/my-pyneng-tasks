f7cb3404d3fc5785544e140e8d01c91a417b9981
Altavista42 <AUSemchenko@mail.ru>
2022-01-16 23:28:20 +0300

    Выполнено задание task_12_1.py

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,33 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+
+def ping_ip_addresses(ip_list):
+    """
+    Функция проверяет пингуются ли IP-адреса.
+    ip-list - ожидает в качестве аргумента список ip-адресов.
+    Взвращает кортеж с двумя списками:
+        - список доступных IP-адресов
+        - список недоступных IP-адресов
+    """
+    alive_ip = []
+    unreach_ip = []
+    for ip in ip_list:
+        command = subprocess.run(['ping', '{}'.format(ip)], stdout=subprocess.DEVNULL)
+        if command.returncode == 0:
+            alive_ip.append(ip)
+        else:
+            unreach_ip.append(ip)
+    ip_tuple = (alive_ip, unreach_ip)
+    return ip_tuple
+
+if __name__ == "__main__":
+      ip_list = ["10.0.0.0", "255.255.0.1", "8.8.8.8"]
+      ip_tuple = ping_ip_addresses(ip_list)
+      print(ip_tuple)
+
+
+
+
+
