9492de9838805d10c274b9a77ed4217cf923f2e9
Altavista42 <AUSemchenko@mail.ru>
2022-01-18 00:13:17 +0300

    Выполнено задание task_12_2.py

--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -34,3 +34,36 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+from ipaddress import ip_address
+
+def convert_ranges_to_ip_list(ip_list):
+    """
+    Функция конвертирует список IP-адресов в разных форматах в список, где каждый IP-адрес указан отдельно.
+    ip_list - в качестве аргумента ожидает список, в котором содержатся IP-адреса и/или диапазоны IP-адресов.
+    Возвращает список IP-адресов.
+    """
+    full_list = []
+    for ip in ip_list:
+        if "-" in ip:
+            divide_ip = ip.split("-")
+            first_ip = ip_address(divide_ip[0])
+            last_ip = divide_ip[1]
+            if len(last_ip.split(".")) != 1:
+                last_ip = ip_address(last_ip)
+                range_ip = list(range(int(first_ip), (int(last_ip) + 1)))
+            else:
+                replace_ip = divide_ip[0].split(".")
+                replace_ip[3] = divide_ip[1]
+                last_ip = ip_address(".".join(replace_ip))
+                range_ip = list(range(int(first_ip), (int(last_ip)+1)))
+            for item in range_ip:
+                new_ip = str(ip_address(item))
+                full_list.append(new_ip)
+        else:
+            full_list.append(ip)
+    return full_list
+
+if __name__ == "__main__":
+    print(convert_ranges_to_ip_list(["8.8.8.8", "192.168.0.1-10", "172.10.0.1-172.10.0.15"]))
+
+
