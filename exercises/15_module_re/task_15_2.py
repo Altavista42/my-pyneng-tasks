3a89e6041a4004ee9f018c8d79311bf6f6b8a9bc
Altavista42 <AUSemchenko@mail.ru>
2022-01-25 22:09:45 +0300

    Выполнено задание task_15_2.py

--- exercises/15_module_re/task_15_2.py
+++ exercises/15_module_re/task_15_2.py
@@ -23,9 +23,7 @@
 """
 import re
 
-regex = re.compile(r'(?P<intf>\S+)\s+(?P<IP>\d+\S+|unassigned)'
-					r'(?:\s+\S+){2}\s+(?P<status>up|down|administratively down)'
-					r'\s+(?P<protocol>\S+)')
+
 
 def parse_sh_ip_int_br(file_config):
 	"""
@@ -37,6 +35,9 @@ def parse_sh_ip_int_br(file_config):
 	file_config - ожидает в качестве аргумента вывод команды sh ip int brief.
 	Возвращает список кортежей.
 	"""
+	regex = re.compile(r'(?P<intf>\S+)\s+(?P<IP>\d+\S+|unassigned)'
+					   r'(?:\s+\S+){2}\s+(?P<status>up|down|administratively down)'
+					   r'\s+(?P<protocol>\S+)')
 	with open(file_config) as config:
 		config_list = [line.groups() for line in regex.finditer(config.read())]
 	return config_list
