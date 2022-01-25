c9f78f0782fd97d861f3fb68253d726d9e1b6ab3
Altavista42 <AUSemchenko@mail.ru>
2022-01-25 23:45:49 +0300

    Выполнено задание task_15_5.py

--- exercises/15_module_re/task_15_5.py
+++ exercises/15_module_re/task_15_5.py
@@ -26,3 +26,26 @@ description Connected to SW1 port Eth 0/1
 
 Проверить работу функции на файле sh_cdp_n_sw1.txt.
 """
+import re
+
+def generate_description_from_cdp(config_file):
+    """
+    Функция обрабатывает вывод команды show cdp neighbors и генерирует
+    на основании вывода команды описание для интерфейсов.
+    config_file - ожидает в качестве аргумента имя файла с выводом команды show cdp neighbors
+    Возвращает словарь, в котором ключи - имена интерфейсов, а значения - команда задающая описание интерфейса.
+    """
+    regex = re.compile(r'(?P<device>\S+)\s+(?P<local_intf>\w+\s+\d+/\d+).+?(?P<port_id>\w+\s+\d+/\d+)')
+    value_template = 'description Connected to {} port {}'
+    conf_dict = {}
+    with open(config_file) as config:
+        for line in regex.finditer(config.read()):
+            device, local_intf, port_id = line.group('device','local_intf','port_id')
+            conf_dict[local_intf] = value_template.format(device, port_id)
+    return conf_dict
+
+
+if __name__ == '__main__':
+    print(generate_description_from_cdp('sh_cdp_n_sw1.txt'))
+
+
