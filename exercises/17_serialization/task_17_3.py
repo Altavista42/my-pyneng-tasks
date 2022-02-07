a2de18af86adc64a5203689bf564fb457ce334b9
r2d2 <r2d2@deb-11-test.jamer.net>
2022-02-07 15:43:54 +0300

    Выполнено задание task_17_3.py

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,3 +24,32 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+import re
+
+
+def parse_sh_cdp_neighbors(config):
+	"""
+	Функция обрабатывает вывод команды show cdp neighbors.
+	config - ожидает в качестве аргумента вывод команды одной строкой.
+	Функция возвращает словарь, который описывает соединения между устройствами.
+	"""
+	device = re.compile(r'(?P<main>\S+)>')
+	neighbor = re.compile(r'(?P<ID>\S+)\s+(?P<local>\S+\s+\d+/\d+).+?(?P<port_id>\w+\s+\d+/\d+)')
+	conf_dict = {}
+	for line in device.finditer(config):
+		main_id = line.group('main')
+		conf_dict[main_id] = {}
+	for line in neighbor.finditer(config):
+		ID, local, port_id = line.group('ID', 'local', 'port_id')
+		conf_dict[main_id][local] = {}
+		conf_dict[main_id][local][ID] = port_id
+	return conf_dict
+
+
+
+
+
+if __name__=="__main__":
+	with open('sh_cdp_n_sw1.txt') as f:
+		print(parse_sh_cdp_neighbors(f.read()))
+
